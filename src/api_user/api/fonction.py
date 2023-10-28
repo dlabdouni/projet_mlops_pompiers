3ddaf59ae7fcfae3ef6858e724cded96c911c295
Willy <130974329+WillyMaillot87@users.noreply.github.com>
2023-10-28 18:24:52 +0200

    Update fonction.py & api_user (from Djamel)

--- src/api_user/api/fonction.py
+++ src/api_user/api/fonction.py
@@ -6,3 +6,14 @@ def format_time(minutes: int) -> str:
         minute_str = f"{minutes} mins"
 
     return minute_str
+
+def format_real_time(minutes: int, seconds: int) -> str:
+    minute_str = "1 min" if minutes == 1 else f"{minutes} mins"
+    second_str = "1 sec" if seconds == 1 else f"{seconds} secs"
+
+    if minutes == 0:
+        return second_str
+    elif seconds == 0:
+        return minute_str
+    else:
+        return f"{minute_str} {second_str}"
